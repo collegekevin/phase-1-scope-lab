0c408425dbe994d3837096d7fa9067707b7feb4d
Kevin Price <collegekevin@gmail.com>
2022-10-13 14:44:24 -0700

    commit tree dcf229b08c5d8faa24587e8bd4ddf63f1b3fa79f

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+const leastFavoriteCustomer = 'kevin';
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    //var bestCustomer = 'not bob';
+    return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+   // var bestCustomer = 'maybe bob';
+    return bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer =  "bob"
+}
